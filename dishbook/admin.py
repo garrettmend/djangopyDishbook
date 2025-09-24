f81d35073347fcc80cce37e5ba62744221bfb1a2
garrettmend <u1305528@umail.utah.edu>
2025-09-23 18:51:51 -0600

    p2
    p2

--- dishbook/admin.py
+++ dishbook/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
+
+class IngredientInline(admin.TabularInline):
+    model = models.Ingredient
+    extra = 1
+
+class StepAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]
+
+class StepInline(admin.TabularInline):
+    model = models.Step
+    extra = 1
+    ordering = ['order']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [StepInline]
+
+admin.site.register(models.Recipe, RecipeAdmin)
+admin.site.register(models.Step, StepAdmin)
+admin.site.register(models.Profile)
 
 # Register your models here.
--- dishbook/models.py
+++ dishbook/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(
+        max_length=200
+    )
+    photo = models.ImageField(
+        upload_to="media/",
+        null=True,
+        blank=True
+    )
+    description = models.TextField()
+
+    prep_time_minutes = models.IntegerField()
+    cook_time_minutes = models.IntegerField()
+    serves = models.IntegerField()
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    copied_from = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='copies'
+    )
+
+    featured_on = models.DateField(
+        null=True,
+        blank=True
+    )
+    
+    tags = models.ManyToManyField(
+        'Tag',
+        blank=True
+    )
+
+class Step(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
+    
+    order = models.IntegerField()
+    
+    description = models.TextField()
+
+class Ingredient(models.Model):
+    step = models.ForeignKey('Step', on_delete=models.SET_NULL, null=True)
+    
+    amount = models.FloatField()
+    
+    unit = models.CharField(
+        max_length=20
+    )
+    
+    name = models.CharField(
+        max_length=100
+    )
+    
+class Tag(models.Model):
+    
+    name = models.CharField(
+        max_length=20
+    )
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="media/", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
