612adbaeb83fd7e8a9c841375c8ab1982830373b
garrettmend <u1305528@umail.utah.edu>
2025-09-24 20:42:19 -0600

    p3
    p3

--- dishbook/views.py
+++ dishbook/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from . import models
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
-    return render(request, "index.html", { })
+    recipes = models.Recipe.objects.order_by('-id')[:3]
+    return render(request, "index.html", {"recipes":  recipes})
 
 def recipe(request, recipe_id):
     return render(request, "recipe.html", {"recipe_id": recipe_id})
 
 def search(request):
-    return render(request, "search.html", {})
+    recipes = models.Recipe.objects.all()
+    return render(request, "search.html", {"recipes":recipes})
 
 def profile(request, username):
-    return render(request, "profile.html", {"username": username})
+    author = get_object_or_404(User, username=username)
+    recipes = models.Recipe.objects.filter(author=author)
+    return render(request, "profile.html", {"username": username, "author": author, "recipes": recipes})
 
 def signin(request):
     return render(request, "signin.html", {})
